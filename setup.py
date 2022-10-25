70fa2a214653116f09afad50fdbec26fa2e0ecd1
posampas <piotr.mierzejewski@mail.com>
2022-10-25 20:27:53 +0200

    Create basic project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='infoSpread',
+    version='0.0.1',
+    author='Piotr Mierzejewski',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
